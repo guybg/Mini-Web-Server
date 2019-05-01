e125f889052a9c364783574aa9d41007bfc0b8b2
guybg <guybbe@gmail.com>
2019-05-01 03:01:11 +0300

    Small changes

--- Mini-Web-Server/Trim.cpp
+++ Mini-Web-Server/Trim.cpp
@@ -3,19 +3,19 @@
 
 const string WHITESPACE = " \n\r\t\f\v";
 
-string Trim::leftTrim(const std::string& i_string)
+string Trim::leftTrim(const string& i_string)
 {
 	size_t start = i_string.find_first_not_of(WHITESPACE);
-	return (start == std::string::npos) ? "" : i_string.substr(start);
+	return (start == string::npos) ? "" : i_string.substr(start);
 }
 
-string Trim::rightTrim(const std::string& i_string)
+string Trim::rightTrim(const string& i_string)
 {
 	size_t end = i_string.find_last_not_of(WHITESPACE);
-	return (end == std::string::npos) ? "" : i_string.substr(0, end + 1);
+	return (end == string::npos) ? "" : i_string.substr(0, end + 1);
 }
 
-string Trim::trim(const std::string& i_string)
+string Trim::trim(const string& i_string)
 {
 	return rightTrim(leftTrim(i_string));
 }
